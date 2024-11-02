8e7cf5e4ae2d453f05b65640fe4b61cb7b1b6794
Edson Vitor <edson.vitor42@gmail.com>
2024-11-02 18:40:50 -0300

    feat: config to run scrape in parabellum only on dev mode

--- cron_jobs/app/scraping.py
+++ cron_jobs/app/scraping.py
@@ -1,10 +1,9 @@
 import asyncio
 from shared.db.database import create_table
+from shared.config import PRODUCTION
 from .services.scrape_service import get_productions, get_processingn, get_commercialization, get_importation, get_exportation
 
-async def start_scrape():
-    create_table()
-
+async def start_scrape_in_parabellum():
     await asyncio.gather(
         get_productions(),
         get_processingn(),
@@ -13,5 +12,17 @@ async def start_scrape():
         get_exportation(),
     )
 
+async def start_scrape():
+    await get_productions(),
+    await get_processingn(),
+    await get_commercialization(),
+    await get_importation(),
+    await get_exportation(),
+
 if __name__ == "__main__":
-    asyncio.run(start_scrape())
+    create_table()
+
+    if PRODUCTION:
+        asyncio.run(start_scrape())
+    else:
+        asyncio.run(start_scrape_in_parabellum())
